dab7bcd554331c269eb6c557192b63a6f56591db
vladislav-gavrikov <vlad.gavr0303@gmail.com>
2024-06-04 11:47:52 +1000

    API

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
-import { Amplify } from 'aws-amplify';
-import outputs from '@/amplify_outputs.json';
+import "@/styles/app.css";
+import type { AppProps } from "next/app";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
-const existingConfig = Amplify.getConfig();
-Amplify.configure({
-  ...existingConfig,
-  API: {
-    ...existingConfig.API,
-    REST: outputs.custom.API,
-  },
-});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
